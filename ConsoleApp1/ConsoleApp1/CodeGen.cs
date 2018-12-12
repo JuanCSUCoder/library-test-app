e5b49a567be982d6998deb0ddcb5a3650ceff3cd
TrolkonJC <42752670+TrolkonJC@users.noreply.github.com>
2018-12-12 16:40:05 -0500

    Added Compiler Method

--- ConsoleApp1/ConsoleApp1/CodeGen.cs
+++ ConsoleApp1/ConsoleApp1/CodeGen.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.CodeDom.Compiler;
+
 namespace ConsoleApp1
 {
     public class CodeGen
@@ -11,10 +13,24 @@ namespace ConsoleApp1
         public static string prefile = "using System;using System.Collections.Generic;using System.Linq;using System.Text;using System.Threading.Tasks;using System.Reflection;namespace ConsoleApp1{class ExampleCodeToCompile{static void main(string[] args){";
         public static string midfile = "AppDomain.CurrentDomain.Load(assemble(path).GetName());";
         public static string posfile = "}static Assembly assemble(string path){Assembly a = Assembly.LoadFrom(path);return a;}}}";
+
         public static string GenerateFile(string path, string code) {
             path = "string path = \""+path+"\";";
             string file = prefile + path + midfile + code + posfile;
             return file;
         }
+        public static void Compiler(string file)
+        {
+            CodeDomProvider provedor = CodeDomProvider.CreateProvider("CSharp");
+            CompilerParameters parames = new CompilerParameters();
+            parames.GenerateExecutable = true;
+            parames.OutputAssembly = "test.exe";
+            parames.TreatWarningsAsErrors = false;
+            parames.GenerateInMemory = false;
+
+            CompilerResults comp = provedor.CompileAssemblyFromFile(parames,file);
+
+        }
+
     }
 }
