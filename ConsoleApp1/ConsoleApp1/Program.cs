54b274f6f822a74e789d59241ea690a64eefa3e2
TrolkonJC <42752670+TrolkonJC@users.noreply.github.com>
2018-12-12 15:33:33 -0500

    Added DLL assembly method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace ConsoleApp1
 {
@@ -13,7 +14,15 @@ namespace ConsoleApp1
             Console.WriteLine("Esta aplicación sirve para probar librerias!!");
             Console.ReadKey();
 
+            Console.Write("Enter the path of the dll file: ");
+            AppDomain.CurrentDomain.Load(assemble(Console.ReadLine()).GetName());
+
             //Codigo para probar las funciones de una libreria
+        }
+
+        static Assembly assemble(string path) {
+            Assembly a = Assembly.LoadFrom(path);
+            return a;
         }
     }
 }
